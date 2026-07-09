028ab70d03d1e1f6006274e24d2eaeca4cfdcd91
Pagalbro007 <161556568+Pagalbro007@users.noreply.github.com>
2026-07-09 19:40:29 +0530

    Add project files.

---
+++ string manipulation and access (day-1)/string_manipulation_and_access__day_1_.py
@@ -0,0 +1,12 @@
+#concatination of strings
+
+first_name="john"
+second_name="durairaj"
+full_name=first_name + " " + second_name
+print(full_name)
+
+#repetation of strings
+
+greeting="Hello " *3
+print(greeting)
+
